efdc585520a3763837baa27947d35a7894e99fda
hukuuma <nikita62017@gmail.com>
2024-04-14 23:47:38 +0500

    fix(rout): delete dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import { GOODS_URL, REVIEWS_URL } from './lib/consts';
 import ShoppingCart from "./ui/shopping-cart";
+import ProductsGrid from './ui/products-grid';
 import FeedbackCard from "./ui/cards/feedback-card";
 import ProductsCard from "./ui/cards/products-card";
 import styles from "./page.module.css";
 
-export default function Home() {
+async function getServerSideReviews() {
+  const reviews = await axios.get(REVIEWS_URL)
+        .then((res) => res.data)
+        .catch((err) => {
+          console.error(err);
+        })
+
+  return reviews
+}
+
+async function getServerSideProducts() {
+  const products = await axios.get(GOODS_URL(1))
+        .then((res) => res.data)
+        .catch((err) => {
+          console.error(err);
+        })
+
+  return products
+}
+
+
+export default async function Home() {
+  const products = await getServerSideProducts();
+  const reviews = await getServerSideReviews();
+
   return (
     <main className={styles.main}>
       <div className={styles.contentWrapper}>
@@ -11,13 +39,12 @@ export default function Home() {
           <p className={styles.mainTitle}>тестовое задание</p>
         </div>
         <div className={styles.feedback}>
-          <FeedbackCard />
-          <FeedbackCard />
+          {reviews && reviews.map((review: any, index: number) => {
+            return <FeedbackCard data={review.text} key={index} />
+          })}
         </div>
         <ShoppingCart />
-        <div className={styles.productsGrid}>
-          <ProductsCard />
-        </div>
+        <ProductsGrid firstPage={products.products}/>
       </div>
     </main>
   );
